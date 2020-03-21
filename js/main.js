df9d13e3df01bdf2aba49a4398f8b1292f6cee42
Paulo Lucas Cerqueira <paulolucas.2010@hotmail.com>
2020-03-21 19:54:58 -0300

    Header finalizado v1.0

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ var typed = $(".typed");
 
 $(function() {
   typed.typed({
-    strings: ["Paulo Lucas.", "Student.", "Developer."],
+    strings: ["Paulo Lucas.", "Student.", "Focused.", "Developer."],
     typeSpeed: 100,
     loop: true,
   });
