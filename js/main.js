88cacb3cac12c5ec23733a71b70b76b0c0eead96
Paulo Lucas Cerqueira <paulolucas.2010@hotmail.com>
2020-03-23 17:56:00 -0300

    Adicionado token de acesso ao GitHub API

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
 $(document).ready(function(){
-  const perfil = "https://api.github.com/users/paulo-lucas";
-  const repositorios = "https://api.github.com/users/paulo-lucas/repos";
+  //const perfil = "https://api.github.com/users/paulo-lucas";
+  const url = "https://api.github.com/users/paulo-lucas/repos";
+  const client_id = "3d29b9344df142ce5d29";
+  const client_secret = "6f5e3561009098ee603ffe39bb771c593578e741";
+  var autenticado = url + "?client_id=" + client_id + "&client_secret=" + client_secret;
+
 
   animaTyped();
 
-  $.get(repositorios, function( data ) {
+  $.get(autenticado, function( data ) {
+      fotoPerfil(data[0].owner.avatar_url);
       data.forEach(addProjeto);
       estilizaCarrossel();
-  });
-
-  $.get(perfil, function(data){
-      fotoPerfil(data.avatar_url);
       $(document).scrollTop(0);
   });
 });
+
