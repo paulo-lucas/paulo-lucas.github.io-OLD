1068a6e4c0e6ef9f86ed63f14430743ca12ec355
Paulo Lucas Cerqueira <paulolucas.2010@hotmail.com>
2020-03-23 00:43:54 -0300

    JavaScript organizado/fotoPerfil atualizada da api

---
+++ js/functions.js
@@ -0,0 +1,54 @@
+// setta src da imagem de perfil
+function fotoPerfil(image){
+  var foto = $("#foto-perfil");
+  foto.attr("src", image);
+}
+
+// efeito do titulo do home
+function animaTyped(){
+  var typed = $(".typed");
+  typed.typed({
+    strings: ["Paulo Lucas.", "Student.", "Focused.", "Developer."],
+    typeSpeed: 100,
+    loop: true,
+  });
+}
+
+// cria carrossel de projetos
+function estilizaCarrossel(){
+  $(".projects-carousel").slick({
+    centerMode: true,
+    centerPadding: '40px',
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    variableWidth: true,
+  });
+}
+
+// adiciona box com informações do projeto
+function addProjeto(repo){
+  var projetos = $(".projects-carousel");
+  var linguagem = repo.language;
+  var homepage = repo.homepage;
+  if(linguagem == null){ linguagem = "outra"; }
+  
+  if(homepage == null || homepage == ""){
+    projetos.append(`<div class="repo-block">
+                      <h3><span class="repo-name">${repo.name}</span></h3>
+                      <div class="description-box shadow p-3"><p disabled class="repo-description">${repo.description}</p></div>
+                      <p class="repo-language">Linguagem: <span>${linguagem}</span></p>
+                      <a href="${repo.html_url}" target="_blank"><span class="badge badge-light">Ver no GitHub</span></a>
+                      
+                  </div> `);
+  }else{
+    projetos.append(`<div class="repo-block">
+                      <h3><span class="repo-name">${repo.name}</span></h3>
+                      <div class="description-box shadow p-3"><p disabled class="repo-description">${repo.description}</p></div>
+                      <p class="repo-language">Linguagem: <span>${linguagem}</span></p>
+                      <a href="${repo.html_url}" target="_blank"><span class="badge badge-light">Ver no GitHub</span></a>
+                      <a href="${homepage}" target="_blank"><span class="badge badge-danger">Ver na página</span></a>
+                  </div> `);
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,84 +1,16 @@
-const url = "https://api.github.com/users/paulo-lucas/repos"
-var xhr = new XMLHttpRequest();
+$(document).ready(function(){
+  const perfil = "https://api.github.com/users/paulo-lucas";
+  const repositorios = "https://api.github.com/users/paulo-lucas/repos";
 
-xhr.open("GET", url);
-xhr.addEventListener("load", function () {
-  if (xhr.status == 200) {
-    var repos = JSON.parse(this.responseText);
-    repos.forEach(addProjeto);
-    estilizaCarrossel();
-  } else {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-  }
-});
-xhr.send();
+  animaTyped();
 
-// efeito do titulo do home
-var typed = $(".typed");
-$(function () {
-  typed.typed({
-    strings: ["Paulo Lucas.", "Student.", "Focused.", "Developer."],
-    typeSpeed: 100,
-    loop: true,
+  $.get(repositorios, function( data ) {
+      data.forEach(addProjeto);
+      estilizaCarrossel();
+  });
+
+  $.get(perfil, function(data){
+      fotoPerfil(data.avatar_url);
+      $(document).scrollTop(0);
   });
 });
-
-
-function estilizaCarrossel(){
-
-  /*
-  $('.projects-carousel').owlCarousel({
-      loop:true,
-      nav: true,
-      responsive: { 0: { 
-          items: 1, 
-        }, 
-        768: { 
-          items: 2, 
-        }, 
-        900: { 
-          items: 4,
-        } 
-      }
-  });*/
-
-  $(".projects-carousel").slick({
-    centerMode: true,
-    centerPadding: '40px',
-    dots: true,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    variableWidth: true,
-  });
-}
-
-
-function addProjeto(repo){
-  var projetos = $(".projects-carousel");
-  var linguagem = repo.language;
-  var homepage = repo.homepage;
-  if(linguagem == null){ linguagem = "outra"; }
-  
-  if(homepage == null || homepage == ""){
-    projetos.append(`<div class="repo-block">
-                      <h3><span class="repo-name">${repo.name}</span></h3>
-                      <div class="description-box shadow p-3"><p disabled class="repo-description">${repo.description}</p></div>
-                      <p class="repo-language">Linguagem: <span>${linguagem}</span></p>
-                      <a href="${repo.html_url}" target="_blank"><span class="badge badge-light">Ver no GitHub</span></a>
-                      
-                  </div> `);
-  }else{
-    projetos.append(`<div class="repo-block">
-                      <h3><span class="repo-name">${repo.name}</span></h3>
-                      <div class="description-box shadow p-3"><p disabled class="repo-description">${repo.description}</p></div>
-                      <p class="repo-language">Linguagem: <span>${linguagem}</span></p>
-                      <a href="${repo.html_url}" target="_blank"><span class="badge badge-light">Ver no GitHub</span></a>
-                      <a href="${homepage}" target="_blank"><span class="badge badge-danger">Ver na página</span></a>
-                  </div> `);
-  }
-
-  
-  
-}
