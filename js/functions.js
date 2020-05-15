d363e9b381e130b2f9cb8ddbf7cbd0c9e346b152
Paulo Cerqueira <paulolucas.2010@hotmail.com>
2020-05-15 18:58:45 -0300

    Atualizações no CV e nos projetos

--- js/functions.js
+++ js/functions.js
@@ -1,11 +1,11 @@
 // setta src da imagem de perfil
-function fotoPerfil(image){
+function fotoPerfil(image) {
   var foto = $("#foto-perfil");
   foto.attr("src", image);
 }
 
 // efeito do titulo do home
-function animaTyped(){
+function animaTyped() {
   var typed = $(".typed");
   typed.typed({
     strings: ["Paulo Lucas.", "Student.", "Focused.", "Developer."],
@@ -15,7 +15,7 @@ function animaTyped(){
 }
 
 // cria carrossel de projetos
-function estilizaCarrossel(){
+function estilizaCarrossel() {
   $(".projects-carousel").slick({
     centerMode: true,
     centerPadding: '40px',
@@ -28,30 +28,39 @@ function estilizaCarrossel(){
 }
 
 // adiciona box com informações do projeto
-function addProjeto(repo){
+function addProjeto(repo) {
   var projetos = $(".projects-carousel");
   var linguagem = repo.language;
   var homepage = repo.homepage;
   var name = repo.name;
 
-  if(linguagem == null){ linguagem = "outra"; }
-  if(name == "paulo-lucas.github.io"){ name = "Esta página"; }
-  
-  if(homepage == null || homepage == ""){
-    projetos.append(`<div class="repo-block">
+  if(name === 'CurriculumVitae') return;
+
+  if (linguagem == null) { linguagem = "outra"; }
+  if (name == "paulo-lucas.github.io") { name = "Esta página"; }
+
+  projetos.append(`<div class="repo-block">
                       <h3><span class="repo-name">${name}</span></h3>
                       <div class="description-box shadow p-3"><p class="repo-description">${repo.description}</p></div>
                       <p class="repo-language">Linguagem: <span>${linguagem}</span></p>
                       <a href="${repo.html_url}" target="_blank"><span class="badge badge-github badge-pill">Ver no GitHub</span></a>
-                      
                   </div> `);
-  }else{
-    projetos.append(`<div class="repo-block">
-                      <h3><span class="repo-name">${name}</span></h3>
-                      <div class="description-box shadow p-3"><p class="repo-description">${repo.description}</p></div>
-                      <p class="repo-language">Linguagem: <span>${linguagem}</span></p>
-                      <a href="${repo.html_url}" target="_blank"><span class="badge badge-pill">Ver no GitHub</span></a>
-                      <a href="${homepage}" target="_blank"><span class="badge badge-pill">Ver na página</span></a>
-                  </div> `);
+
+  
+  if(homepage && homepage != ""){
+    $('.repo-block:last-child').append(`
+      <a href="${homepage}" target="_blank"><span class="badge badge-pill">Ver na página</span></a>
+    `)
   }
+
+  if(name == "be-the-hero" 
+    || name == 'catalogo-de-passaros' 
+    || name == 'drawit'
+    || name == 'github-tracker'
+    || name == 'Esta página') 
+    {
+      $('.repo-block:last-child').prepend(`
+        <img class="star-emoji" src="https://img.icons8.com/emoji/96/000000/star-emoji.png"/>
+      `)
+    }
 }
